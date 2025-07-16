7a16a82bf0e8c3fa9de196d4c8c993b71b2fc797
Анатолий Степанов <anatolijstepanov@MacBook-Air-Anatolij.local>
2025-07-16 18:28:03 +0800

    Переключен OAuth на серверную обработку для решения проблемы CORS

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import TikTokCallback from "./pages/TikTokCallback";
+import TikTokCallbackServer from "./pages/TikTokCallbackServer";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/oauth/callback" element={<TikTokCallback />} />
+          <Route path="/oauth/callback" element={<TikTokCallbackServer />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
